c386f8e641cb085503de80c61625ea24566bead4
martip23 <patrickmartinez77@gmail.com>
2017-08-13 17:27:31 -0500

    Added js to change background image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () { "use strict";
                                $("#coin-button").on("click", function () {
         $("#quote").html("Between a one and a three is always a two.");
         $("#author").html("Native American Proverb");
-        $("#background").css("background-image", "url(../media/caravan.jpg)");
-        $("#content-container").css("background-color", "rgba(204,102,0,0.8)");
+        $("#background").css("backgroundImage", "url(../media/caravan.jpg)");
+        $("#content-container").css("backgroundColor", "rgba(204,102,0,0.8)");
     });
     });
